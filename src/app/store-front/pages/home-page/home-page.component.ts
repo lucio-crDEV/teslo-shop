0a5ab323bd828dd8d5940f723f7b342364fa645e
lucio-crDEV <lucio.m.cardenas@gmail.com>
2025-06-27 21:44:10 -0400

    estructura inicial, configuracion de importaciones y dependencias

---
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ProductCardComponent } from "../../../products/components/product-card/product-card.component";
+
+@Component({
+  selector: 'app-home-page',
+  imports: [ProductCardComponent],
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent { }
