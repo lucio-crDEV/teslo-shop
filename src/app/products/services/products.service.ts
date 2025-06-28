b0d4036800509420454e308dbd46df07ee443775
lucio-crDEV <lucio.m.cardenas@gmail.com>
2025-06-28 11:19:32 -0400

    Implementando peticion GET en nuestros services con los providers configurados

---
+++ src/app/products/services/products.service.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { ProductsResponse } from '../interfaces/product.interface';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class ProductsService {
+
+    private http = inject(HttpClient);
+
+    getProducts(): Observable<ProductsResponse> {
+        return this.http.get<ProductsResponse>(
+            'http://localhost:3000/products'
+        ).pipe(
+            tap(resp => console.log)
+        )
+    }
+
+}
