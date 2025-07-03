fb40d220169905fd836d8344128eb8e6c19424a6
lucio-crDEV <lucio.m.cardenas@gmail.com>
2025-07-03 02:44:33 -0400

    uso de cache implementado en product.service para evitar consultar multiples en la api. Aplicación manual, se recomienda revisa a user algun paquete para manejo masivo de mayor complejidad (proyectos reales) como: TankStack Query

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { count, Observable, of, tap } from 'rxjs';
 
 import { Product, ProductsResponse } from '@products/interfaces/product.interface';
 import { environment } from 'src/environments/environment';
@@ -18,10 +18,24 @@ export class ProductsService {
 
     private http = inject(HttpClient);
 
+    // El cache de products almacena un Map que se genera con el tipado ProductsResponse
+    // Se genera una variable privada que alamacena un Map() compuestos por un par definido en el tipado string, ProductsResponse
+    private productsCache = new Map<string, ProductsResponse>()
+
     getProducts(options: Options): Observable<ProductsResponse> {
 
-        // Desestructurando las opciones para definir valores por defecto mientras siguen prototipado de la interface
+        // Valores por defecto para las querys 
         const { limit = 9, offset = 0, gender = '' } = options;
+
+        // estructura de la key<string> para el par <string, ProductsResponse> del productsCache
+        const key = `${limit}-${offset}-${gender}` // 9-0-''
+
+        // evaluando si productsCache contiene la key(llave) definida arriba
+        if (this.productsCache.has(key)) {
+            // si la contiende corta la ejecución con un return y devuelve un Observable con los valores almacenados en caché
+            return of(this.productsCache.get(key)!)
+        }
+
 
         return this.http
             .get<ProductsResponse>(`${baseUrl}/products`, {
@@ -32,14 +46,14 @@ export class ProductsService {
                 }
             })
             .pipe(
-                tap((resp) => {
-                    console.log(resp)
-                })
+                tap((resp) => console.log(resp)),
+                // se almacena la response de la consulta en caché, para la implementación anterior que evita consultas masivas en el backend
+                tap((resp) => this.productsCache.set(key, resp)),
             );
     }
 
     getProductByIdSlug(idSlug: string): Observable<Product> {
 
-        return this.http.get<Product>(`${baseUrl}/products/${ idSlug }`);
+        return this.http.get<Product>(`${baseUrl}/products/${idSlug}`);
     }
 }
