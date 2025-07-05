9242ab3ccb04de21818bec965c39bf966ece0851
lucio-crDEV <lucio.m.cardenas@gmail.com>
2025-07-05 18:31:24 -0400

    componentes creados login/register cargando atraves del routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
 
     {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes')
+    },
+
+    {
         path: '',
         loadChildren: () => import('./store-front/store-front.routes'),
-    }
-
-
+    },
 ];
