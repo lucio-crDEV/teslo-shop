a0992aee4ed1614d9cda8dee46bc7c97d60454b2
lucio-crDEV <lucio.m.cardenas@gmail.com>
2025-07-28 14:02:09 -0400

    formulario reactivo para login implementado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,9 @@ export const routes: Routes = [
     {
         path: 'auth',
         loadChildren: () => import('./auth/auth.routes')
+        
     },
-
+    // orden importante, path vacío debiese ser el último para que pase en primer luegar las rutas específicamente definidas
     {
         path: '',
         loadChildren: () => import('./store-front/store-front.routes'),
